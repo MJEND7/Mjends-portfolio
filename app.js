6374cdb21a57229357786f221c49ec2fab53c758
MJEND7 <60278279+MJEND7@users.noreply.github.com>
2021-03-29 12:45:34 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 function F1 () {
     document.getElementById("h1").innerHTML = "this is my github portfolio!";
-    document.getElementById("p1").innerHTML = '<a href="https://github.com/MJEND7/Mjends-portfolio">https://github.com/MJEND7/Mjends-portfolio</a>'
+    document.getElementById("p1").innerHTML = '<a href="https://github.com/MJEND7/Mjends-portfolio" target="_blank">github.com/MJEND7/Mjends-portfolio</a>'
     
+}
+
+function F2 () {
+    document.getElementById("h1").innerHTML = "this is my codepen work!";
+    document.getElementById("p1").innerHTML = '<a href="https://codepen.io/mjend7" target="_blank">codepen.io/mjend7</a>'
+}
+
+function F3 () {
+    document.getElementById("h1").innerHTML = "this is my fivver work!";
+    document.getElementById("p1").innerHTML = '<a href="https://www.fiverr.com/jeffcoder566?public_mode=true/" target="_blank">www.fiverr.com/jeffcoder566?public_mode=true</a>'
+
 }
