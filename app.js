9af9c1f123c4b2f357f43eefcb3ca0185b7fde77
MJEND7 <60278279+MJEND7@users.noreply.github.com>
2021-03-29 12:03:50 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function F1 () {
+    document.getElementById("h1").innerHTML = "this is my github portfolio!";
+    document.getElementById("p1").innerHTML = '<a href="https://github.com/MJEND7/Mjends-portfolio">https://github.com/MJEND7/Mjends-portfolio</a>'
+    
+}
