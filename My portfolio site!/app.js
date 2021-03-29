5f123f5ff594e3c71c0993f164bccc4662722182
MJEND7 <60278279+MJEND7@users.noreply.github.com>
2021-03-29 12:03:26 +1000

    Delete My portfolio site! directory

--- My portfolio site!/app.js
+++
@@ -1,5 +0,0 @@
-function F1 () {
-    document.getElementById("h1").innerHTML = "this is my github portfolio!";
-    document.getElementById("p1").innerHTML = '<a href="https://github.com/MJEND7/Mjends-portfolio">https://github.com/MJEND7/Mjends-portfolio</a>'
-    
-}
