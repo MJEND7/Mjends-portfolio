f7349311e711b845e60cbe52dfb572fd0a9ff7cc
MJEND7 <60278279+MJEND7@users.noreply.github.com>
2021-04-03 17:06:56 +1000

    Create app.js

---
+++ Crow 64 Translator/app.js
@@ -0,0 +1,113 @@
+// Please note this code sucks and i made it in 20 minutes
+
+function A(){
+var A = document.getElementById('display');
+A.insertAdjacentHTML("beforebegin", "A");
+}
+function B(){
+var B = document.getElementById('display');
+B.insertAdjacentHTML("beforebegin", 'B')
+}
+function C(){
+var C = document.getElementById('display');
+C.insertAdjacentHTML("beforebegin", 'C')
+}
+function D(){
+var D =document.getElementById('display');
+D.insertAdjacentHTML("beforebegin", 'D')
+}
+function E(){
+var E = document.getElementById('display');
+E.insertAdjacentHTML("beforebegin", 'E')
+}
+function F(){
+var F = document.getElementById('display');
+F.insertAdjacentHTML("beforebegin", 'F')
+}
+function G(){
+var G = document.getElementById('display');
+G.insertAdjacentHTML("beforebegin", 'G')
+}
+function H(){
+var H = document.getElementById('display');
+H.insertAdjacentHTML("beforebegin", 'H')
+}
+function I(){
+var I = document.getElementById('display');
+I.insertAdjacentHTML("beforebegin", 'I')
+}
+function J(){
+var J = document.getElementById('display');
+J.insertAdjacentHTML("beforebegin", 'J')
+}
+function K(){
+var K = document.getElementById('display');
+K.insertAdjacentHTML("beforebegin", 'K')
+}
+function L(){
+var L = document.getElementById('display');
+L.insertAdjacentHTML("beforebegin", 'L')
+}
+function M(){
+var M = document.getElementById('display');
+M.insertAdjacentHTML("beforebegin", 'M')
+}
+function N(){
+var N = document.getElementById('display');
+N.insertAdjacentHTML("beforebegin", 'N')
+}
+function O(){
+var O = document.getElementById('display');
+O.insertAdjacentHTML("beforebegin", 'O')
+}
+function P(){
+var P = document.getElementById('display');
+P.insertAdjacentHTML("beforebegin", 'P')
+}
+function Q(){
+var Q = document.getElementById('display');
+Q.insertAdjacentHTML("beforebegin", 'Q')
+}
+function R(){
+var R = document.getElementById('display');
+R.insertAdjacentHTML("beforebegin", 'R')
+}
+function S(){
+var S = document.getElementById('display');
+S.insertAdjacentHTML("beforebegin", 'S')
+}
+function T(){
+var T = document.getElementById('display');
+T.insertAdjacentHTML("beforebegin", 'T')
+}
+function U(){
+var T = document.getElementById('display');
+T.insertAdjacentHTML("beforebegin", 'U')
+}
+function V(){
+var V = document.getElementById('display');
+V.insertAdjacentHTML("beforebegin", 'V')
+}
+function W(){
+var W = document.getElementById('display');
+W.insertAdjacentHTML("beforebegin", 'W')
+}
+function Y(){
+var Y = document.getElementById('display');
+Y.insertAdjacentHTML("beforebegin", 'Y')
+}
+function X(){
+var X = document.getElementById('display');
+X.insertAdjacentHTML("beforebegin", 'X')
+}
+function Z(){
+var Z = document.getElementById('display');
+Z.insertAdjacentHTML("beforebegin", 'Z')
+}
+function space(){
+var space = document.getElementById('display');
+space.insertAdjacentHTML("beforebegin", '      ')
+}
+function refreshPage(){
+    window.location.reload();
+} 
