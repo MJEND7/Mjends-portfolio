92d81db938ead9fc8b78022ad8ff6c27b9572f09
MJEND7 <60278279+MJEND7@users.noreply.github.com>
2021-06-03 17:10:52 +1000

    ✨comments

---
+++ wired effects/app.js
@@ -0,0 +1,5 @@
+// This is what the room looks like 
+// on drugs. dont do Drugs or D.D.D
+//heres the codepen! https://codepen.io/mjend7/pen/vYxWxzP
+//base css from Misha Tsankashvili
+//Edits by MJEND
